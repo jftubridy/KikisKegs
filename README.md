cf265c11b0ef7acd0a37cdef899f42e15c6f6b5e
Joseph Tubridy <jftubridy@gmail.com>
2020-02-14 16:53:39 -0800

    route structure to README

---
+++ README.md
@@ -0,0 +1,34 @@
+# Kiki's Keg House
+### By Joseph Tubridy
+
+## Description
+*  _This is a Web app that displays and tracks the beverages available at a tap house._
+
+## Specs
+
+<img src="src/assets/images.RouteStructure.jpg>
+
+## Setup/Installation Requirements
+
+* _Clone this repository._
+* _Type "npm install" in your console._
+* _Type "npm run start" in your console._
+* _Navigate to https://localhost:8080/ in your browser._
+
+## Known Bugs
+
+_Currently one can only navigate between pages, there is no functionality to create, edit, or delete kegs._
+
+## Technologies Used
+
+_JavaScript_
+_React_
+_webpack_
+_npm_
+
+
+### License
+
+*This is licensed under the MIT License.*
+
+Copyright (c) 2019 **_Joseph Tubridy_**
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,40 +5,11 @@ import { withRouter } from 'react-router-dom';
 
 function AboutUs() {
   var myStyles = {
-    color: 'blue',
+    color: 'white',
     fontSize: '30px',
-    borderColor: 'white',
-    textShadowColor: 'white',
-    textShadowOffset: { width: 5, height: 5 },
-    textShadow: {
-      fontSize: 30,
-      color: 'black',
-      textShadowColor: 'white',
-      testShadowOffset: { width: 5, height: 5 },
-      textShadowRadius:10,
-    },
-    borderStyle: 'solid',
-    borderWidth: 3,
-    borderRadius: 600
   };
   return (
-    <div><style jsx> {`
-    p {
-      color: black;
-    }
-    p:hover {
-      color: orange;
-      font-size: 30px;
-      text-shadow: 0 0 3px #FF0444. 0 0 5px #0000FF;
-      textShadow: {
-        font-size: 30;
-        color: black;
-        text-shadow-color: white;
-        text-shadow-offset: { width: 5, height: 5 };
-        text-shadow-radius:10
-      },
-    }
-    `}</style>
+    <div>
       <h1>Owner</h1>
       <img src={Bartender} />
       <p style={myStyles}>Hi I'm Kiki.  In my youth I owned a delivery service, but now I run this wonderful Tap House.  We have an exstensive and growing list of beverages on tap, and I'm willing to fly to remote locations to obtain even the most obscure refreshments!</p>
